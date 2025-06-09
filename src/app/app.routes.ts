1f43738832233a646ba51745513aad0ca30b28f1
Roberds <robertodiazgs@gmail.com>
2025-06-09 16:42:30 +0200

    se añaden rutas hijas del dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,24 @@ export const routes: Routes = [
 
   {
     path: 'dashboard',
-    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component')
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component')
+      },
+      {
+        path:'**',
+        redirectTo: 'trending'
+      }
+    ]
   },
-  {
-    path: 'trending',
-    loadComponent: () => import('./gifs/pages/trending-page/trending-page.component')
-  },
-  {
-    path: 'search',
-    loadComponent: () => import('./gifs/pages/search-page/search-page.component')
-  },
+
   {
     path: '**',
     redirectTo: 'dashboard'
